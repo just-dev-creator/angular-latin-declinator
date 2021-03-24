64233329b1f5981936d6ee97fa57c6afb907db3e
just-dev-creator <justus@justus-alvermann.de>
2021-03-24 16:09:31 +0100

    Fixed eslint errors

--- declinator/src/app/form/form.component.ts
+++ declinator/src/app/form/form.component.ts
@@ -1,7 +1,8 @@
 /*
  * File: form.component.ts
  * Project: declinator
- * Copyright: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 import { Byte } from '@angular/compiler/src/util';
@@ -15,117 +16,120 @@ import { NgModel } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
   @Input()
-  wortstamm: String | undefined;
+  wortstamm: string | undefined;
   @Input()
+  // eslint-disable-next-line @typescript-eslint/ban-types
   onKey!: Function;
   @Input()
-  kasus: String | undefined;
+  kasus: string | undefined;
   @Input()
   nummerus: Byte | undefined;
   @Input()
-  gender: String | undefined;
+  gender: string | undefined;
   @Input()
+  // eslint-disable-next-line @typescript-eslint/ban-types
   calculateValues!: EventEmitter<Function>;
 
-  entry: String | undefined;
+  entry: string | undefined;
 
   ngOnInit(): void {
+    // eslint-disable-next-line @typescript-eslint/ban-types
     this.calculateValues.subscribe((calculateFunction: Function) => {
-      if (this.entry != undefined) {
+      if (this.entry !== undefined) {
         calculateFunction(this.entry, this.kasus, this.nummerus);
       }
     });
   }
 
-  get getVal(): String {
+  get getVal(): string {
     if (!this.wortstamm) {return '';}
-    else if (this.kasus == 'nom') {
-      if (this.nummerus == 0) {
-        if (this.gender == 'm') {
+    else if (this.kasus === 'nom') {
+      if (this.nummerus === 0) {
+        if (this.gender === 'm') {
           return this.wortstamm + 'us';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'a';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'um';
         }
       } else {
-        if (this.gender == 'm') {
+        if (this.gender === 'm') {
           return this.wortstamm + 'i';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'ae';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'a';
         }
       }
-    } else if (this.kasus == 'gen') {
-      if (this.nummerus == 0) {
-        if (this.gender == 'm') {
+    } else if (this.kasus === 'gen') {
+      if (this.nummerus === 0) {
+        if (this.gender === 'm') {
           return this.wortstamm + 'i';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'ae';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'i';
         }
       } else {
-        if (this.gender == 'm') {
+        if (this.gender === 'm') {
           return this.wortstamm + 'orum';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'arum';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'orum';
         }
       }
-    } else if (this.kasus == 'dat') {
-      if (this.nummerus == 0) {
-        if (this.gender == 'm') {
+    } else if (this.kasus === 'dat') {
+      if (this.nummerus === 0) {
+        if (this.gender === 'm') {
           return this.wortstamm + 'o';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'ae';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'o';
         }
       } else {
-        if (this.gender == 'm') {
+        if (this.gender === 'm') {
           return this.wortstamm + 'is';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'is';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'is';
         }
       }
-    } else if (this.kasus == 'akk') {
-      if (this.nummerus == 0) {
-        if (this.gender == 'm') {
+    } else if (this.kasus === 'akk') {
+      if (this.nummerus === 0) {
+        if (this.gender === 'm') {
           return this.wortstamm + 'um';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'am';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'um';
         }
       } else {
-        if (this.gender == 'm') {
+        if (this.gender === 'm') {
           return this.wortstamm + 'os';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'as';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'a';
         }
       }
-    } else if (this.kasus == 'abl') {
-      if (this.nummerus == 0) {
-        if (this.gender == 'm') {
+    } else if (this.kasus === 'abl') {
+      if (this.nummerus === 0) {
+        if (this.gender === 'm') {
           return this.wortstamm + 'o';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'a';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'o';
         }
       } else {
-        if (this.gender == 'm') {
+        if (this.gender === 'm') {
           return this.wortstamm + 'is';
-        } else if (this.gender == 'f') {
+        } else if (this.gender === 'f') {
           return this.wortstamm + 'is';
-        } else if (this.gender == 'n') {
+        } else if (this.gender === 'n') {
           return this.wortstamm + 'is';
         }
       }
@@ -133,34 +137,34 @@ export class FormComponent implements OnInit {
     return '';
   }
 
-  get placeholder(): String {
+  get placeholder(): string {
     if (this.wortstamm) {return '';}
-    else if (this.kasus == 'nom') {
-      if (this.nummerus == 0) {
+    else if (this.kasus === 'nom') {
+      if (this.nummerus === 0) {
         return 'servus';
       } else {
         return 'servi';
       }
-    } else if (this.kasus == 'gen') {
-      if (this.nummerus == 0) {
+    } else if (this.kasus === 'gen') {
+      if (this.nummerus === 0) {
         return 'servi';
       } else {
         return 'servorum';
       }
-    } else if (this.kasus == 'dat') {
-      if (this.nummerus == 0) {
+    } else if (this.kasus === 'dat') {
+      if (this.nummerus === 0) {
         return 'servo';
       } else {
         return 'servis';
       }
-    } else if (this.kasus == 'akk') {
-      if (this.nummerus == 0) {
+    } else if (this.kasus === 'akk') {
+      if (this.nummerus === 0) {
         return 'servum';
       } else {
         return 'servos';
       }
-    } else if (this.kasus == 'abl') {
-      if (this.nummerus == 0) {
+    } else if (this.kasus === 'abl') {
+      if (this.nummerus === 0) {
         return 'servo';
       } else {
         return 'servis';
